07eb01371e8a163e1a1455e18a1f265c1c2c605c
pureawesome <gennaro.the.younger@gmail.com>
2015-03-19 11:47:07 -0700

    First commit
    Simple dynamic counter taking variables for jump and speed

---
+++ jquery.countUp.js
@@ -0,0 +1,38 @@
+(function ($) {
+  $.fn.countUp = function (options) {
+    options = options || {};
+
+    options = $.extend({
+            jump: 5,
+      			speed: 3
+        }, options);
+
+    var message = "test";
+    $('.message').html(message);
+
+    var self = this,
+				$self = $(this),
+				data = parseInt($self.attr('data-startnum')) || 2000,
+				speed,
+        jump;
+
+    function getJump() {
+      return Math.floor(Math.random() * options.jump) + 1;
+    }
+
+    function getSpeed() {
+      return Math.floor(Math.random() * options.speed) + 1;
+    }
+
+    function startCount(){
+      jump = getJump();
+      speed = getSpeed() * 1000;
+      data += jump;
+      $self.html(data);
+      setTimeout(startCount, speed);
+    }
+
+    startCount();
+  }
+
+}(jQuery));
