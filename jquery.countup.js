2c78052b1f3f202b9e3746c7683e84b98e5e0e1d
pureawesome <gennaro.the.younger@gmail.com>
2015-03-19 12:01:24 -0700

    reset

---
+++ jquery.countup.js
@@ -0,0 +1,35 @@
+(function ($) {
+  $.fn.countUp = function (options) {
+    options = options || {};
+
+    options = $.extend({
+            jump: 5,
+      			speed: 5
+        }, options);
+
+    var self = this,
+				$self = $(this),
+				data = parseInt($self.attr('data-startnum')) || 2000,
+				speed,
+        jump;
+
+    function getJump() {
+      return Math.floor(Math.random() * options.jump) + 1;
+    }
+
+    function getSpeed() {
+      return Math.floor(Math.random() * options.speed) + 1;
+    }
+
+    function startCount(){
+      jump = getJump();
+      speed = getSpeed() * 1000;
+      data += jump;
+      $self.html(data);
+      setTimeout(startCount, speed);
+    }
+
+    startCount();
+  }
+
+}(jQuery));
